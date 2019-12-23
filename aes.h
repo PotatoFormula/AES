6c5f46c92e511d7786260a8fbcfe098805227dae
PotatoFormula <PotatoFormula@gmail.com>
2019-12-23 21:57:30 +0800

    public CBC

--- aes.h
+++ aes.h
@@ -15,4 +15,6 @@ void ctx_init_iv(struct aes_ctx* ctx, const uint8_t* key, const uint8_t* iv, uns
 void AES_ECB_encrypt(const struct aes_ctx* ctx, uint8_t* buf);
 void AES_ECB_decrypt(const struct aes_ctx* ctx, uint8_t* buf);
 
+void AES_CBC_encrypt_buffer(struct aes_ctx *ctx, uint8_t *buf, uint32_t buf_len);
+void AES_CBC_decrypt_buffer(struct aes_ctx *ctx, uint8_t *buf, uint32_t buf_len);
 #endif // _AES_h_
